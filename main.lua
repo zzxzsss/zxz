b2eaa4cb7ca52d75120fc1734e31091e2be2da4d
zzxzsss <goodooking911@gmail.com>
2026-01-01 21:36:49 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/f3468d87bd0b8924"))()
+loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/f3468d87bd0b8924"))() 
 -- key 
